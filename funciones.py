643246aabfb6ba43e8e57dc7b4f2cfe13cbabb2a
Carlosmgp4 <carlosmanuelgamezperezdeguzman@gmail.com>
2021-03-04 19:25:12 +0100

    He añadido a las funciones un menu y las funciones relativas a los putos 2 y 3 de la actividad, también he añadido a la aplicación principal el menu y las opciones 2 y 3.

--- funciones.py
+++ funciones.py
@@ -4,9 +4,31 @@ def leer_fichero(fichero):
         datos = json.load(f)
     return datos
 
+def menu():
+    print ("1. Mostrar nombre de todos los campeones")
+    print ("2. Contar el número de tags de cada campeón")
+    print ("3. Mostrar descripción de un campeón")
+    print ("4. Mostrar que campeones pertenecen a un tag")
+    print ("5. Mostrar campeón con la estadística mas elevada")
+    opcion = int(input("Que acción le gustaría realizar: "))
+    return opcion
+
 def listar_campeones(fich):
     lista = []
     for cam in fich:
         campeon = cam.get("name")
         lista.append(campeon)
     return lista
+
+def contar_tag(fich):
+    lista = []
+    for var in fich:
+        tags = len(var.get("tags"))
+        lista.append(tags)
+    return lista
+
+def mostrar_descrip(fich,buscar):
+    lista = []
+    for var in fich:
+        if var.get("name") == buscar:
+            return print (var.get("description"))
--- programa.py
+++ programa.py
@@ -1,2 +1,22 @@
 from funciones import *
 fich = leer_fichero("github/ejercicio_json/Campeoneslol.json")
+
+opcion = menu()
+while opcion != 6:
+    if opcion == 1:
+        for var in listar_campeones(fich):
+            print (var)
+        print()
+    elif opcion == 2:
+        for ca,ta in zip(listar_campeones(fich),contar_tag(fich)):
+            if ta > 1:
+                print (ca,"tiene",ta,"tags")
+            else:
+                print (ca,"tiene",ta,"tag")
+        print ()
+    elif opcion == 3:
+        campe = input("De que campeón desea saber la descripción: ")
+        mostrar_descrip(fich,campe.capitalize())
+        print()
+
+    opcion = menu()
